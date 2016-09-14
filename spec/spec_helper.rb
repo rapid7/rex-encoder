33661ce97c115c68071d2739f352dd1aa5065010
David Maloney <DMaloney@rapid7.com>
2016-09-14 11:03:25 -0500

    move xor encoder shared example over
    copy this shared example over from framework
    and make sure it gets loaded
    
    MS-1708

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,4 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'rex/encoder'
+# Autoload our support files such as shared examples
+Dir["./spec/support/**/*.rb"].sort.each { |f| require f}

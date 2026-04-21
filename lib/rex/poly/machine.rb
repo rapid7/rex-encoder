2bfc07203ca924a6d13a6cfba7baa620759756bc
adfoster-r7 <alandavid_foster@rapid7.com>
2026-04-21 11:20:49 +0100

    defer loading metasm

--- lib/rex/poly/machine.rb
+++ lib/rex/poly/machine.rb
@@ -1,10 +1,11 @@
 # -*- coding: binary -*-
 
+autoload :Metasm, 'metasm'
+
 module Rex
 
   module Poly
 
-    require 'metasm'
     require 'rex/poly/machine/machine'
     require 'rex/poly/machine/x86'
 
